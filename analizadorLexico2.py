352d64c94ca04ed1e6ba64e04c3d15dfa9bc7f31
Laura Valentina Ceballos Aguilar <93541070+lceballosa1@users.noreply.github.com>
2022-09-13 22:41:59 -0500

    automata terminado

--- analizadorLexico2.py
+++ analizadorLexico2.py
@@ -119,76 +119,71 @@ automata.addEstado("", 0) #1
 automata.addEstado("", 0) #2 
 automata.addEstado("", 0) #3 
 automata.addEstado("tkn_float", 1) #4
-automata.addEstado("tkn_integer", 1) #5
+automata.addEstado("tkn_period", 1) #5 #tener en cuenta
 automata.addEstado("tkn_integer", 1) #6
 automata.addEstado("", 0) #7
 automata.addEstado("tkn_str", 1) #8
+automata.addEstado("", 0) #9
+automata.addEstado("id", 1) #10
+automata.addEstado("", 0) #11 se borra
+automata.addEstado("tkn_assign", 1) #12
+automata.addEstado("", 0) #13
+automata.addEstado("tkn_less", 1) #14 
+automata.addEstado("", 0) #15 se borra
+automata.addEstado("token_dif", 1) #16 se borra
+automata.addEstado("tkn_leq", 1) #17
+automata.addEstado("", 0) #18
+automata.addEstado("tkn_greater", 1) #19 #tener en ceunta que el = puede estar en ambos caminos. Considerar quitarlo del ASCCI
+automata.addEstado("tkn_geq", 1) #20
+automata.addEstado("", 0) #21
+automata.addEstado("tkn_div", 1) #22
+automata.addEstado("", 0) #23
+automata.addEstado("tkn_comment", 1) #24
+automata.addEstado("tkn_opening_par", 1) #25
+automata.addEstado("tkn_closing_par", 1) #26
+automata.addEstado("tkn_plus", 1) #27
+automata.addEstado("", 0) #28
+automata.addEstado("tkn_minus", 1) #29
+automata.addEstado("tkn_closing_bra", 1) #30
+automata.addEstado("tkn_opening_bra", 1) #31
+automata.addEstado("tkn_times", 1) #32
+automata.addEstado("tkn_equal", 1) #33
+automata.addEstado("tkn_mod", 1) #34
+automata.addEstado("tkn_neq", 1) #35 
+automata.addEstado("tkn_semicolon", 1) #36
+automata.addEstado("tkn_comma", 1) #37
+automata.addEstado("tkn_question_mark", 1) #38 OJO
 
 #NUMEROS
 #Se agrega al estado 0 del automata las aristas que permiten pasar del estado "" a "" con UN número.
 for i in range(48,58):
   automata.addArista(0, chr(i), 1)
+  automata.addArista(1, chr(i), 1)
+  automata.addArista(2, chr(i), 3)
+  automata.addArista(3, chr(i), 3)
 
 #ASCII
 #Se agregan todas los caracteres de ASCII al estado 1 para llegar al estado 6 (tkn_integer)
 for i in range(256):
   automata.addArista(1, chr(i), 6)
-
-#NÚMEROS
-#Se agregan todos los número en el estado 1 y permiten llegar de nuevo al estado 1 para generar un entero gigante :v .
-for i in range(48,58):
-  automata.addArista(1, chr(i), 1)
-
+  automata.addArista(2, chr(i), 5)
+  automata.addArista(3, chr(i), 4)
+  automata.addArista(7, chr(i), 7)
+  automata.addArista(13, chr(i), 14)
+  automata.addArista(18, chr(i), 19) 
+  automata.addArista(28,chr(i),12)  #assing
+  automata.addArista(21, chr(i), 22)
+  automata.addArista(23, chr(i), 23)
+ 
 #PUNTO
 #Se toma en cuenta para los token de tipo flotante.
 automata.addArista(1, chr(46), 2)
 
-#ASCII
-#Se agregan los caracteres de ASCII al estado 2 para llegar al estado 5 (tkn_integer)
-for i in range(256):
-  automata.addArista(2, chr(i), 5)
-
-#NÚMERO
-#Se agregan los números desde el estado 2 al 3 para determinar si realmente es un flotante.
-for i in range(48, 58):
-  automata.addArista(2, chr(i), 3)
-
-
-#ASCII
-#Se agregan ASCII  de estado 3 a 4 (tkn_float)
-for i in range(255):
-  automata.addArista(3, chr(i), 4)
-
-#NÚMEROS
-#Se agregan los bucles en el estado tres con los números
-for i in range(48,58):
-  automata.addArista(3, chr(i), 3)
-
-
+#Manejo Strings ""
 # " 
 automata.addArista(0, chr(34), 7)
-
-# ''
-#automata.addArista(0, chr(39), 7)
-
-#ASCII
-#Se agrega el alfabeto para trabajar los strings
-for i in range(255):
-  automata.addArista(7, chr(i), 7)
-
-# "
 #Se agrega estado para finalizar el str y llega al estado (tkn_str)
 automata.addArista(7, chr(34), 8)
-
-# ''
-#automata.addArista(7, chr(39), 8)
-
-
-automata.addEstado("", 0) #9
-automata.addEstado("id", 1) #10
-automata.addEstado("token_neg", 1) #11
-automata.addEstado("token_igual", 1) #12
-
 
 #letras en mayúscula a estado "" y bucle de estos
 for i in range(65, 91):
@@ -210,88 +205,81 @@ for i in range(48,58):
 for i in range(256):
   automata.addArista(9, chr(i), 10)
 
-  
+#Caracteres especiales:
 
-automata.addArista(9, "_", 9)
+# _
+automata.addArista(9, chr(95), 9)
+# =
+automata.addArista(0, chr(61), 28)
 
-automata.addArista(0, "~", 11)
+# ==
+automata.addArista(28, chr(61), 33)
 
-automata.addArista(0, "=", 12)
+# !=
+automata.addArista(0, chr(33)+chr(61), 35)
 
-automata.addEstado("", 0) #13
-automata.addEstado("token_menor", 1) #14
-automata.addEstado("token_asig", 1) #15
-automata.addEstado("token_dif", 1) #16
-automata.addEstado("token_menor_igual", 1) #17
+# < 
+automata.addArista(0, chr(60), 13)
 
-automata.addArista(0, "<", 13)
+# <=
+automata.addArista(13, chr(61), 17)
 
-for i in range(256):
-  automata.addArista(13, chr(i), 14)
+# >
+automata.addArista(0, chr(62), 18)
 
-automata.addArista(13, "-", 15)
+# >=
+automata.addArista(18, chr(61), 20)
 
-automata.addArista(13, ">", 16)
+# /
+automata.addArista(0,  chr(92), 21)
 
-automata.addArista(13, "=", 17)
+# /
+automata.addArista(21, chr(92), 23)
 
-automata.addEstado("", 0) #18
-automata.addEstado("token_mayor", 1) #19
-automata.addEstado("token_mayor_igual", 1) #20
-automata.addEstado("", 0) #21
-automata.addEstado("token_div", 1) #22
-automata.addEstado("", 0) #23
-automata.addEstado("token_comentario", 1) #24
-
-automata.addArista(0, ">", 18)
-
-for i in range(256):
-  automata.addArista(18, chr(i), 19)
-automata.addArista(18, "=", 20)
-
-automata.addArista(0, "/", 21)
-
-for i in range(256):
-  automata.addArista(21, chr(i), 22)
-automata.addArista(21, "/", 23)
-
-for i in range(256):
-  automata.addArista(23, chr(i), 23)
+#LF nueva linea para comprobar que es un comentario
 automata.addArista(23, chr(10), 24)
 
-automata.addEstado("token_par_izq", 1) #25
-automata.addEstado("token_par_der", 1) #26
-automata.addEstado("token_mas", 1) #27
-automata.addEstado("token_o", 1) #28
-automata.addEstado("token_menos", 1) #29
-automata.addEstado("token_cor_der", 1) #30
-automata.addEstado("token_cor_izq", 1) #31
-automata.addEstado("token_mul", 1) #32
-automata.addEstado("token_y", 1) #33
-automata.addEstado("token_mod", 1) #34
-automata.addEstado("token_pot", 1) #35
-automata.addEstado("token_pyc", 1) #36
-automata.addEstado("token_coma", 1) #37
-automata.addEstado("token_dosp", 1) #38
+# (
+automata.addArista(0, chr(40), 25)
 
-automata.addArista(0, "(", 25)
-automata.addArista(0, ")", 26)
-automata.addArista(0, "+", 27)
-automata.addArista(0, "|", 28)
-automata.addArista(0, "-", 29)
-automata.addArista(0, "]", 30)
-automata.addArista(0, "[", 31)
-automata.addArista(0, "*", 32)
-automata.addArista(0, "&", 33)
-automata.addArista(0, "%", 34)
-automata.addArista(0, "^", 35)
-automata.addArista(0, ";", 36)
-automata.addArista(0, ",", 37)
-automata.addArista(0, ":", 38)
+# )
+automata.addArista(0, chr(41), 26)
 
+# +
+automata.addArista(0, chr(43), 27)
+
+# -
+automata.addArista(0, chr(45), 29) 
+
+# ]
+automata.addArista(0, chr(93), 30)
+
+# [
+automata.addArista(0, chr(91), 31)
+
+# *
+automata.addArista(0, chr(42), 32)
+
+# %
+automata.addArista(0, chr(37), 34) 
+
+# ;
+automata.addArista(0,chr(59), 36)
+
+# ,
+automata.addArista(0, chr(44), 37)
+
+# ,
+automata.addArista(0, chr(44), 38)
+
+
+#Salto de linea
 automata.addArista(0, chr(10), 0)
-automata.addArista(0, chr(13), 0)
-automata.addArista(0, " ", 0)
+
+automata.addArista(0, chr(13), 0) # ?
+
+# cadena vacía
+automata.addArista(0, " ", 0) 
 
 
 
